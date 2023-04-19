8e18625492e2df10ac1a664c092801cf1586e10c
Kevincito <Key_park2121@hotmail.com>
2023-04-19 16:48:50 -0600

    first commit Kevin

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <link rel="icon" type="icon/png" href="https://cdn.pixabay.com/photo/2018/05/03/21/49/android-3372580_1280.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="index.css">
+	<title>Community Manager Proyect</title>
+    </head>
+    <body>
+     <!--Seccion del login-->
+     <section class="Home">
+     	<div class="Navbar">
+      		<p>Logo</p>
+      		
+      	</div>
+      	<div class="Nav">
+      		<p>Logo</p>
+      		
+      	</div>
+      
+	  <div class="cover"> 
+     
+      <!-- seccion contendo dentro de la tabala-->
+
+      </div class="Contenido de la tabla">
+      <h1>¡Crea tu cuenta gratis!</h1>
+      <p>Nombre</p><br><br>
+        <p>Email</p><br><br>
+         <p>Contaseña</p><br><br>
+          <button class="btn-primary">Create Account</button><br><br>
+           <p>Already have a account? Log in</p><br><br>
+           <p>-OR-</p><br><br>
+           
+      <div> 
+      	<button class="btn-secundary">Ingresa con Google</button><br> 
+      	<br> <button class="btn-secundary">Ingresa con facebook</button><br><br>
+           
+      </div>   
+      </section>
+
+
+
+     
+     </body>
+    </html>
